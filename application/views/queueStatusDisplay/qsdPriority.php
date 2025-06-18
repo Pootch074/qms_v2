5ce9251fb34e86d40d876bf0fe31cb3cc52cd1cc
Sacrudyme <bladsparrow74@gmail.com>
2025-06-18 13:53:31 +0800

    hide the dummy table

--- application/views/queueStatusDisplay/qsdPriority.php
+++ application/views/queueStatusDisplay/qsdPriority.php
@@ -157,155 +157,65 @@
 
 
             <!-- TO BE HIDDEN -->
-            <div class="col-6 text-center border d-flex flex-column">
+            <div id="mnbghjk" class="col-6 text-center border d-flex flex-column">
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS2W1Prio" style="width:60%;">
                     </div>
-
                     <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
-                        <!-- <h2 class="qsdStepFont">1</h2> -->
                     </div>
-
-                    <!-- <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">1</h2>
-                    </div> -->
                 </div>
 
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS2W2Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">2</h2>
-                    </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">2</h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
 
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS3W1Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">
-                            3
-                        </h2>
-                    </div>
-
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">
-                            1
-                        </h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
 
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS3W2Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">
-                            3
-                        </h2>
-                    </div>
-
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">
-                            2
-                        </h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
 
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS3W3Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">
-                            3
-                        </h2>
-                    </div>
-
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">
-                            3
-                        </h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS3W4Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">
-                            3
-                        </h2>
-                    </div>
-
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">
-                            4
-                        </h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
 
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
-
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS4W1Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">
-                            4
-                        </h2>
-                    </div>
-
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">
-                            1
-                        </h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
 
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
-
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS4W2Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">
-                            4
-                        </h2>
-                    </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">
-                            2
-                        </h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
                 <div class="cardCstm flex-grow-1 mb-1 d-flex align-items-center justify-content-center" style="width:100%;">
-
                     <div class="h-100 d-flex align-items-center justify-content-center" id="hiddenQsdS4W3Prio" style="width:60%;">
                     </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="width:20%;">
-                        <h2 class="qsdStepFont">
-                            4
-                        </h2>
-                    </div>
-
-                    <div class="h-100 d-flex align-items-center justify-content-center" style="margin-right:2vw; width:20%;">
-                        <h2 class="qsdStepFont">
-                            3
-                        </h2>
+                    <div class="h-100 d-flex align-items-center justify-content-center" id="abcdcallCount" style="width:20%;">
                     </div>
                 </div>
             </div>
